73a25b59d4e85ffc8ecb87bce356c2eb9791b7eb
Johannes Sandjaja <sandjajajohannes@gmail.com>
2020-10-11 12:37:54 +0700

    add handling, gitignore

--- beautiful-soup-example.py
+++ beautiful-soup-example.py
@@ -1,11 +1,17 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = input("Enter - ")
-print(url)
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
+url = input("Enter URL (example - http://www.youtube.com ) : ")
+try:
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
 
-tags = soup("a")
-for tag in tags:
-    print(tag.get("href", None))
+    tags = soup("a")
+    if len(tags) == 0:
+        print("Cannot found link with tag <a> in", url)
+        quit()
+    for tag in tags:
+        print(tag.get("href", None))
+except:
+    print("Something went wrong")
+    quit()
